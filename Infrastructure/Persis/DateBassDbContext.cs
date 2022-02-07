9a912f46d6ac616395358c76080a306fce029414
baghermohammad@hotmail.com <baghermohammad@hotmail.com>
2022-02-07 23:55:01 +0330

    add folders infrasutrure and dadcontext

---
+++ Infrastructure/Persis/DateBassDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Persis
+{
+    public class DateBassDbContext 
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DateBassDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("SqlConnection");
+        }
+        public IDbConnection CreateConnection()
+            => new SqlConnection(_connectionString);
+
+
+    }
+}
